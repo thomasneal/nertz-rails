5b5c04bfae7c583b51382df132972e0be0bd747d
Thomas Neal <thomasevanneal@gmail.com>
2018-09-06 16:25:27 -0400

    Add devise for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+     @user = User.find(params[:id])
+  end
   def new
      @user = User.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180905193517) do
+ActiveRecord::Schema.define(version: 20180906000058) do
 
   create_table "games", force: :cascade do |t|
     t.string "status"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20180905193517) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
