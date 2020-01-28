68acd4598306ed70a9a070833d65fa88cb0faa89
Tom Neal <thomasevanneal@gmail.com>
2020-01-27 21:37:39 -0500

    Auto sort columns based off who is winning

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,9 +3,22 @@ class Game < ApplicationRecord
   has_many :rounds, dependent: :destroy
   has_many :scores, through: :rounds
 
+  def last_round
+    rounds.last
+  end
+
+  def ordered_users
+    users_list = []
+    the_scores = last_round.scores.order_highest
+    the_scores.each do |s|
+      users_list.push(s.user)
+    end
+    return users_list
+  end
+
   def isFinished
     scores = []
-    
+
     self.users.each do |u|
       scores.push(self.scores.where(user: u).pluck(:value).sum)
     end
--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,6 @@ class Score < ApplicationRecord
   belongs_to :user
 
   validates :value, presence: true, numericality: {only_integer: true}
+
+  scope :order_highest, -> { reorder(value: :desc) }
 end
